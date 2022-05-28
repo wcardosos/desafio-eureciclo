bd5ba9d088bbf5db78c502fd86d1c68bbc628d62
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-28 15:01:54 -0300

    feat(sales/models) Add create method

--- src/sales/models.py
+++ src/sales/models.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-member
 '''
     Sale app models.
 '''
@@ -8,3 +9,30 @@ class Sale(models.Model):
     '''
         Model to represent a sale.
     '''
+    buyer = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    address = models.CharField(max_length=255)
+    provider = models.CharField(max_length=255)
+
+    @classmethod
+    def create(cls, sale_data: dict) -> None:
+        '''
+            Create a sale.
+        '''
+        buyer = sale_data['buyer']
+        description = sale_data['description']
+        price = sale_data['price']
+        quantity = sale_data['quantity']
+        address = sale_data['address']
+        provider = sale_data['provider']
+
+        cls.objects.create(
+            buyer=buyer,
+            description=description,
+            price=price,
+            quantity=quantity,
+            address=address,
+            provider=provider,
+        )
