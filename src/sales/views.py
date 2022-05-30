59b06ded1d00ba749eb633f67ccf571b64e094b7
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-30 12:52:15 -0300

    feat(sales/views) Add save and render imported info on result view

--- src/sales/views.py
+++ src/sales/views.py
@@ -3,7 +3,7 @@
     Sales views.
 '''
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from lib.file_handler import FileHandler
@@ -61,5 +61,28 @@ class ResultView(TemplateView):
     '''
     template_name = 'result.html'
 
-    def post(self, *args, **kwargs) -> HttpResponse:
-        return render(self.request, 'result.html')
+    def get(self, *args, **kwargs) -> HttpResponse:
+        '''
+            Save the sales and render the results.
+        '''
+        self.request.session.modified = True
+        sales_info = self.request.session.get('sales_info')
+
+        if not sales_info:
+            return redirect('/sales')
+
+        Sale.create_from_list(sales_info)
+
+        total_imported_sales = Sale.get_total_imported_sales(sales_info)
+        total_price_from_imported_sales = Sale.get_total_price_from_imported_sales(  # noqa: E501
+            sales_info
+        )
+
+        results = {
+            'total_imported_sales': total_imported_sales,
+            'total_price': total_price_from_imported_sales
+        }
+
+        self.request.session['sales_info'] = None
+
+        return render(self.request, 'result.html', results)
--- tests/sales/views/test_result_view.py
+++ tests/sales/views/test_result_view.py
@@ -12,10 +12,33 @@ class TestProcessingView(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_result(self):
+    def test_result_without_sales_info_session(self):
         '''
-            Should render the import results.
+            Should to redirect to the homepage
+            when the sales informations are not
+            in the session request.
         '''
         response = self.client.get('/sales/processing/result')
 
+        self.assertRedirects(response, '/sales', target_status_code=301)
+
+    def test_results(self):
+        '''
+            Should render the import sales result.
+        '''
+        session = self.client.session
+        session['sales_info'] = [{
+            'buyer': "View Test",
+            'description': "description",
+            'price': 10.0,
+            'quantity': 2,
+            'address': "address",
+            'provider': "provider"
+        }]
+        session.save()
+
+        response = self.client.get('/sales/processing/result')
+
         self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.context['total_imported_sales'])
+        self.assertIsNotNone(response.context['total_price'])
