4f1cd7b1de3e8e44bf692d689d485f9a5d77cffa
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-30 17:32:02 -0300

    feat(sales/views) Add error handling

--- src/sales/views.py
+++ src/sales/views.py
@@ -1,10 +1,17 @@
-# pylint: disable=no-member
+# pylint: disable=no-member,import-error
 '''
     Sales views.
 '''
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from errors.lib.txt_parser.invalid_file_content_exception import (
+    InvalidFileContentException
+)
+from errors.lib.txt_parser.invalid_line_content_exception import (
+    InvalidLineContentException
+)
+from errors.lib.txt_parser.parse_exception import ParseException
 
 from lib.file_handler import FileHandler
 from lib.txt_parser import TxtParser
@@ -22,10 +29,14 @@ class HomeView(TemplateView):
         sales_count = Sale.get_count()
         sales_total_price = Sale.get_total_price()
 
+        error = self.request.session.get('import_sales_error')
+        self.request.session['import_sales_error'] = None
+
         data = {
             'last_sale': last_sale,
             'sales_count': sales_count,
-            'sales_total_price': sales_total_price
+            'sales_total_price': sales_total_price,
+            'error': error
         }
 
         return render(self.request, 'index.html', data)
@@ -41,14 +52,28 @@ class ProcessingView(TemplateView):
         '''
             Process the import sales file.
         '''
+        error = ''
         file_handler = FileHandler()
         txt_parser = TxtParser()
 
-        sales_info = Sale.compose_from_file(
-            self.request.FILES['sales'],
-            file_handler,
-            txt_parser
-        )
+        try:
+            sales_info = Sale.compose_from_file(
+                self.request.FILES['sales'],
+                file_handler,
+                txt_parser
+            )
+        except InvalidFileContentException:
+            error = 'Conteúdo do arquivo inválido'
+        except InvalidLineContentException:
+            error = 'Dados das vendas inválidos'
+        except ParseException:
+            error = 'Erro ao parsear os dados do arquivo'
+        except Exception as error:
+            error = f'Aconteceu um erro desconhecido. Descrição: {str(error)}'
+
+        if error:
+            self.request.session['import_sales_error'] = error
+            return redirect('/sales')
 
         self.request.session['sales_info'] = sales_info
 
