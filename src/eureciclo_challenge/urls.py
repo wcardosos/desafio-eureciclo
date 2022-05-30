ec1d21408659ce2e3e324cfd2338dffeb11ed24c
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-30 13:08:55 -0300

    feat(urls) Add empty path redirect to sales page

--- src/eureciclo_challenge/urls.py
+++ src/eureciclo_challenge/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sales/', include('sales.urls', namespace='sales'))
+    path('sales/', include('sales.urls', namespace='sales')),
+    path('', RedirectView.as_view(url='/sales', permanent=True))
 ]
