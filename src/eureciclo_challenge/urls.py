d5bf420933840c2c0009fbbf60b1b1765f70b523
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-31 11:52:12 -0300

    fix(urls) Remove redirect that was not working

--- src/eureciclo_challenge/urls.py
+++ src/eureciclo_challenge/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sales/', include('sales.urls', namespace='sales')),
-    path('', RedirectView.as_view(url='/import', permanent=True))
+    path('sales/', include('sales.urls', namespace='sales'))
 ]
