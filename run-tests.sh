d2ce404e6a70bf3c5c3a5c1869efb6ed3eeee528
Wagner Cardoso <wagnerdev01@gmail.com>
2022-05-30 23:12:54 -0300

    feat() Add tests coverage

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
 docker-compose up -d
-docker-compose exec web-django python src/manage.py test
+
+if [ ${1:-""} = "coverage" ]; then
+    docker-compose exec web-django coverage run --source='.' src/manage.py test
+    docker-compose exec web-django coverage report
+else
+    docker-compose exec web-django python src/manage.py test
+fi
